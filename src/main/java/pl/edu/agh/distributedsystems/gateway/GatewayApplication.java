ac8271dd020e7cb2e95dc035969893e31aefec4d
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-11-16 23:53:53 +0100

    add pom and main service class

---
+++ src/main/java/pl/edu/agh/distributedsystems/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.distributedsystems.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+}
